b2b04efb634c8c1507dad7ce0c120832d37464e5
yash gupta <yash63966736@gmail.com>
2024-12-06 00:59:51 +0530

    add new files

---
+++ menu/manu.js
@@ -0,0 +1,25 @@
+let div1=document.querySelector("#div1");
+div1.addEventListener("click",()=>{
+
+   document.location.href='../chest/chest.html'
+})
+let div2=document.querySelector("#div2");
+div2.addEventListener("click",()=>{
+
+   document.location.href='../back/back.html'
+})
+let div3=document.querySelector("#div3");
+div3.addEventListener("click",()=>{
+
+   document.location.href='../leg/leg.html'
+})
+let div4=document.querySelector("#div4");
+div4.addEventListener("click",()=>{
+
+   document.location.href='../shoulder/shoulder.html'
+})
+let div5=document.querySelector("#div5");
+div5.addEventListener("click",()=>{
+
+   document.location.href='../bicep/bicep.html'
+})
